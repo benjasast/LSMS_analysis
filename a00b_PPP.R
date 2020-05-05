518fd0a414a5944424a4939346aa300c19052dd4
Ben Sas Trakinsky <bsastrakinsky@wluservices-MacBook-Pro-2.local>
2020-05-05 12:11:26 -0400

    PPP adjustments done

--- a00b_PPP.R
+++ a00b_PPP.R
@@ -69,7 +69,7 @@ adjustment_ready %>% filter(is.na(adjustment))
 tab_adjustment <- adjustment_ready %>% 
   mutate(adjustment = case_when(
     survey=="Bosnia_2004" ~ `PA.NUS.PPP`/ 84.5/103.6713, # Information from CPI from FRED, 2010 base that is why is divided
-    survey=="Bosnia_2001" ~ `PA.NUS.PPP` / 84.5/ (1+(0.547/100))*(1+(0.313/100))*(1+(4.573/100)) /103.6713 ,
+    survey=="Bosnia_2001" ~ `PA.NUS.PPP` / 84.5/  103.6713 / (1+(0.547/100))/(1+(0.313/100))/(1+(4.573/100)) ,
     survey=="Ghana_1989" ~ 108.7268 / cpi/	1.054099e-02, # used closest year for PPP ER 1990
     survey=="Ghana_1988" ~ 108.7268 / cpi/	1.054099e-02, # used closest year for PPP ER 1990
     TRUE ~ adjustment # all the rest
@@ -79,22 +79,146 @@ tab_adjustment <- adjustment_ready %>%
 
 # Adjust tidydatasets -----------------------------------------------------
 
-
-
 # Tidy dataset
 df_tidy_adjusted <- df_tidy %>% left_join(tab_adjustment, by="survey") %>% 
-  mutate_at(c("food_consumption","nonsub_consumption","nonhealth_consumption","nonfood_nohealth_consumption","oops"),funs(. /adjustment))
+  mutate_at(c("food_consumption","nonsub_consumption","nonhealth_consumption","nonfood_nohealth_consumption","oops"),funs(. /adjustment)) 
 
-tab <- df_tidy_adjusted %>% 
+# Make manual adjustments -------------------------------------------------
+
+
+# Fixes for Iraq (survey measured in 1000s)
+df_iraq_adjusted <- df_tidy_adjusted %>% 
+  filter(country=="Iraq") %>% 
+  mutate_at(c("food_consumption","nonsub_consumption","nonhealth_consumption","nonfood_nohealth_consumption","oops"),funs(. * 1000))
+
+df_iraq_adjusted %>% 
+  group_by(survey,module) %>% 
+  summarise(mean_oops = mean(oops, na.rm = TRUE)) %>% 
+  mutate(mean_oops = mean_oops %>% round(2) )
+
+# Fixes for Ghana 2006 - they used old cedis instead of new
+df_ghana2006_adjusted <- adjustment_ready %>% 
+  filter(survey=="Ghana_2006") %>% 
+  mutate(adjustment = 5.4/ 9259.259259 / cpi) %>%  # PPP/forex/inflation
+left_join(df_tidy, by="survey") %>% 
+  mutate_at(c("food_consumption","nonsub_consumption","nonhealth_consumption","nonfood_nohealth_consumption","oops"),funs(. *adjustment)) 
+
+df_ghana2006_adjusted %>% group_by(survey,module) %>% 
+  summarise(mean_oops = mean(oops, na.rm = TRUE))
+
+# Fixes Ghana 1991
+df_ghana1991_adjusted <- adjustment_ready %>% 
+  filter(survey=="Ghana_1991") %>% 
+  mutate(adjustment = 5.4/ (166.06/100)*1.431 / cpi) %>%   # PPP/effective exchange rate (2010=100) * forex 2010/inflation
+  left_join(df_tidy, by="survey") %>% 
+  mutate_at(c("food_consumption","nonsub_consumption","nonhealth_consumption","nonfood_nohealth_consumption","oops"),funs(. /adjustment)) 
+
+df_ghana1991_adjusted %>% group_by(survey,module) %>% 
+  summarise(mean_oops = mean(oops, na.rm = TRUE))
+
+# Fixes Ghana 1989
+df_ghana1989_adjusted <- adjustment_ready %>% 
+  filter(survey=="Ghana_1989") %>% 
+  mutate(adjustment = 5.4/ (163.86/100)*1.431 / cpi) %>%   # PPP/effective exchange rate (2010=100) * forex 2010/inflation
+  left_join(df_tidy, by="survey") %>% 
+  mutate_at(c("food_consumption","nonsub_consumption","nonhealth_consumption","nonfood_nohealth_consumption","oops"),funs(. /adjustment)) 
+
+df_ghana1989_adjusted %>% group_by(survey,module) %>% 
+  summarise(mean_oops = mean(oops, na.rm = TRUE))
+
+# Fixes Ghana 1988
+df_ghana1988_adjusted <- adjustment_ready %>% 
+  filter(survey=="Ghana_1988") %>% 
+  mutate(adjustment = 5.4/ (175.15/100)*1.431 / cpi) %>%   # PPP/effective exchange rate (2010=100) * forex 2010/inflation
+  left_join(df_tidy, by="survey") %>% 
+  mutate_at(c("food_consumption","nonsub_consumption","nonhealth_consumption","nonfood_nohealth_consumption","oops"),funs(. /adjustment)) 
+
+df_ghana1988_adjusted %>% group_by(survey,module) %>% 
+  summarise(mean_oops = mean(oops, na.rm = TRUE))
+
+# Fixes Bosnia 2004
+df_bosnia2004_adjusted <- adjustment_ready %>% 
+  filter(survey=="Bosnia_2004") %>% 
+  mutate(adjustment =  1/ (1.5579*(84.5/103.6713)*0.876)      ) %>%   # divided by exchange rate, CPI, and PPP adjustment
+  left_join(df_tidy, by="survey") %>% 
+  mutate_at(c("food_consumption","nonsub_consumption","nonhealth_consumption","nonfood_nohealth_consumption","oops"),funs(. *adjustment)) 
+
+df_bosnia2004_adjusted %>% group_by(survey,module) %>% 
+  summarise(mean_oops = mean(oops, na.rm = TRUE))
+
+# Fixes Bosnia 2004
+df_bosnia2004_adjusted <- adjustment_ready %>% 
+  filter(survey=="Bosnia_2004") %>% 
+  mutate(adjustment =  1/ (1.76*(84.5/103.6713)*0.876)      ) %>%   # divided by exchange rate, CPI, and PPP adjustment
+  left_join(df_tidy, by="survey") %>% 
+  mutate_at(c("food_consumption","nonsub_consumption","nonhealth_consumption","nonfood_nohealth_consumption","oops"),funs(. *adjustment)) 
+
+df_bosnia2004_adjusted %>% group_by(survey,module) %>% 
+  summarise(mean_oops = mean(oops, na.rm = TRUE))
+
+# Fixes Bosnia 2001
+cpi_2001_bosnia <-103.6713 / (1+(0.547/100))/(1+(0.313/100))/(1+(4.573/100))
+
+df_bosnia2001_adjusted <- adjustment_ready %>% 
+  filter(survey=="Bosnia_2001") %>% 
+  mutate(adjustment =  1/ ((1.5579*(84.5/cpi_2001_bosnia)*0.876))      ) %>%   # divided by exchange rate, CPI, and PPP adjustment (latest)
+  left_join(df_tidy, by="survey") %>% 
+  mutate_at(c("food_consumption","nonsub_consumption","nonhealth_consumption","nonfood_nohealth_consumption","oops"),funs(. *adjustment)) 
+
+df_bosnia2001_adjusted %>% group_by(survey,module) %>% 
+  summarise(mean_oops = mean(oops, na.rm = TRUE))
+
+
+# Put adjustments into main df --------------------------------------------
+
+corrected_surveys <- c("Bosnia_2001","Bosnia_2004",
+                       "Ghana_1988","Ghana_1989","Ghana_1991","Ghana_2006",
+                       "Iraq_2007","Iraq_2012")
+
+# Put all corrected surveys in one tibble
+list_corrected_surveys <- list(df_bosnia2001_adjusted,df_bosnia2004_adjusted,
+                               df_ghana1988_adjusted,df_ghana1989_adjusted,df_ghana1991_adjusted,df_ghana2006_adjusted,
+                               df_iraq_adjusted) %>% reduce(full_join)
+
+# Eliminate from adjusted dataset all corrected surveys
+'%notin%' <- Negate('%in%')
+
+df_adjusted_well <- df_tidy_adjusted %>% 
+  filter(survey %notin% corrected_surveys)
+
+# Join corrected surveys to adjusted dataset
+df_adjusted_well <- df_adjusted_well %>% 
+  full_join(list_corrected_surveys)
+
+tab <- df_adjusted_well %>% 
   group_by(survey, module) %>% 
   summarise(mean_oops = mean(oops, na.rm = TRUE)) %>% 
   mutate(mean_oops = mean_oops %>% round(2) )
 
 tab
 
+# Save Adjusted Df --------------------------------------------------------
+
+df_tidy_adjusted <- df_adjusted_well %>% 
+  rename(year = year.x) %>% 
+  select(-cpi,-PA.NUS.PPP,-adjustment,-year.y)
+
+head(df_tidy_adjusted)
+
+df_nontidy_adjusted <- df_tidy_adjusted %>% 
+  #group_by(hhid_compilation) %>% 
+  mutate(obs = row_number()) %>% 
+  pivot_wider(names_from = module, values_from = oops) %>% 
+  group_by(hhid_compilation) %>% 
+  mutate(Health = max(Health, na.rm = FALSE),
+         Consumption = max(Consumption, na.rm = FALSE)) %>% 
+  slice(1)
 
 
-
+# Tidy adjusted
+write_csv2(df_tidy_adjusted, "LSMScompilation_tidy_PPPadjusted.csv")
+# Non-Tidy adjusted
+write.csv2(df_nontidy_adjusted, "LSMScompilation_nontidy_PPPadjusted.csv")
 
 
 
